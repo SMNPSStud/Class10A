6c671326c1ecf08ff922cab0977884a2d9e5b622
SMNPSStud <96935495+SMNPSStud@users.noreply.github.com>
2022-01-04 08:52:19 +0530

    Add files via upload

--- P1_T.py
+++ P1_T.py
@@ -11,27 +11,29 @@ w1 = pygame.Rect(100,150,50,50)
 b2 = pygame.Rect(150,150,50,50)
 w2 = pygame.Rect(150,100,50,50)
 
-b3 = pygame.Rect(200,100,50,50)
-w3 = pygame.Rect(200,150,50,50)
+b3 = pygame.rect(140,150,40,50)
+w3 = pygame.rect(160,160,60,60)
 
-b4 = pygame.Rect(250,150,50,50)
-w4 = pygame.Rect(250,100,50,50)
+b4 = pygame.rect(140,140,40,40)
+w4 = pygame.rect(140,150,50,40)
+
+
 while True:    
     screen.fill((150,75,0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            sys.exit()
     
     pygame.draw.rect(screen,(0,0,0),b1)
-    pygame.draw.rect(screen,(255,255,255),w1)
+    pygame.draw.rect(screen,(255, 255, 255),w1)
     
     pygame.draw.rect(screen,(0,0,0),b2)
-    pygame.draw.rect(screen,(255,255,255),w2)
+    pygame.draw.rect(screen,(255, 255, 255), w2)
     
-    pygame.dram.rect(screen,(0,0,0),b3)
-    pygame.draw.rect(screen,(255,255,255),w3)
+    pygame.draw.rect(screen,(0,0,0),b3)
+    pygame.draw.rect(screen,(255, 255, 255), w3)
     
     pygame.draw.rect(screen,(0,0,0),b4)
-    pygame.draw.rect(screen,(255,255,255),w4)
+    pygame.draw.rect(screen,(255, 255, 255), w4)
     pygame.display.update()
+    
