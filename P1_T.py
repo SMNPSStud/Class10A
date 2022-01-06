041b5320f9b9d11e3f6836c7322afc527e3f0076
SMNPSStud <96935495+SMNPSStud@users.noreply.github.com>
2022-01-06 08:55:05 +0530

    Add files via upload

--- P1_T.py
+++ P1_T.py
@@ -1,39 +1,6 @@
-import pygame,sys 
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  6 08:47:16 2022
 
-pygame.init()
-clock=pygame.time.Clock()
-
-screen = pygame.display.set_mode((600,300))
-
-b1 = pygame.Rect(100,100,50,50)
-w1 = pygame.Rect(100,150,50,50)
-
-b2 = pygame.Rect(150,150,50,50)
-w2 = pygame.Rect(150,100,50,50)
-
-b3= pygame.rect(200,200,50,50)
-w3= pygame.rect(200,250,50,50)
-
-b4= pygame.rect(200,100,50,50)
-w4= pygame.rect(200,150,50,50)
-while True:
+@author: 
     
-    screen.fill((150,75,0))
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-    
-    pygame.draw.rect(screen,(0,0,0),b1)
-    pygame.draw.rect(screen,(255,255,255),w1)
-   
-    pygame.draw.rect(screen,(0,0,0),b2)
-    pygame.draw.rect(screen,(255,255,255),w2)
-    
-    pygame.draw.rect(screen,(0,0,0),b3)
-    pygame.draw.rect(screen,(255,255,255),w3)
-    
-    pygame.draw.rect(screen,(0,0,0),b4)
-    pygame.draw.rect(screen,(255,255,255),w4)
-    
-    pygame.display.update()
