a933edb036bd1a0c6369e4088fedd7593d094938
SMNPSStud <96935495+SMNPSStud@users.noreply.github.com>
2022-01-06 08:46:02 +0530

    Add files via upload

--- P1_T.py
+++ P1_T.py
@@ -11,12 +11,11 @@ w1 = pygame.Rect(100,150,50,50)
 b2 = pygame.Rect(150,150,50,50)
 w2 = pygame.Rect(150,100,50,50)
 
-b3 = pygame.Rect(200,100,50,50)
-w3 = pygame.Rect(200,150,50,50)
+b3 = pygame.Rect (200,100,50,50)
+w3 = pygame.Rect (200,150,50,50)
 
 b4 = pygame.Rect(250,150,50,50)
-w4 = pygame.Rect(250,100,50,50)
-
+w4 = pygame.Rect(250,100,50,50) 
 while True:    
     screen.fill((150,75,0))
     for event in pygame.event.get():
@@ -30,12 +29,11 @@ while True:
     pygame.draw.rect(screen,(0,0,0),b2)
     pygame.draw.rect(screen,(255,255,255),w2)
     
+    
     pygame.draw.rect(screen,(0,0,0),b3)
     pygame.draw.rect(screen,(255,255,255),w3)
     
     pygame.draw.rect(screen,(0,0,0),b4)
     pygame.draw.rect(screen,(255,255,255),w4)
     
-    
-             
     pygame.display.update()
