371e2062482ed8be0b32f20afc8abab8284a1b49
SMNPSStud <96935495+SMNPSStud@users.noreply.github.com>
2022-01-07 08:33:20 +0530

    Add files via upload

--- C4 SA.py
+++ C4 SA.py
@@ -37,9 +37,10 @@ while True:
     pygame.draw.rect(screen, (100, 100, 100), cactus_rect)
     pygame.draw.rect(screen, (100, 100, 100), ground_rect)
     
-    if dino_rect.colliderect (cactus_rect): 
-        pygame.time.delay(2000)
-        pygame.quit()
+    if dino_rect.colliderect (cactus_rect) :
+      pygame.time.delay(2000)
+      pygame.quit()
+    
     pygame.display.update()
     
     
